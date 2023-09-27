b475a3bb1459ea30720b8af8c9c1592c8679dff8
z0nsk1 <nurmjo@gmail.com>
2023-09-27 15:25:05 +0300

    tehtävät 3.2 ja 3.3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const app = express()
+const app = express() // käytetään http:tä parempaa kirjastoa, eli expressiä
 
+// kovakoodattu data sivulle
 let persons = [
     {
         id: 1,
@@ -24,10 +25,32 @@ let persons = [
     }
 ]
 
+let info = `Phonebook has info for ${persons.length} people <br> 
+            ${new Date()}` 
+
+// Haetaan listaus kaikista henkilöistä
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+// Haetaan listaus yksittäisistä henkilöistä id:n perusteella
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id) // req.params.id on merkkijono, muutetaan se luvuksi Number:n avulla, jotta se kelpaa find:lle
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person) // jos hlö löytyy, näytetään
+    } else {
+        res.status(404).end() // muussa tapauksessa error 404 "not found" ja .end ilmoittaa siitä, että pyyntöön tulee vastata ilman dataa
+    }
+})
+
+// Haetaan sivuston infosivu, info määritelty ylempänä
+app.get('/info', (req, res) => {
+    res.send(info)
+})
+
+// Avataan serveri porttiin 3001
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
