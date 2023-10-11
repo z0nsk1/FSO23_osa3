62e4e2c796ebd6d722a613f561af4f7f4fb1732d
z0nsk1 <nurmjo@gmail.com>
2023-10-11 13:59:26 +0300

    3.19-3.21: numeron päivitykseen vielä validoinnit ja sen takia uusi buildi

--- index.js
+++ index.js
@@ -102,13 +102,12 @@ app.delete('/api/persons/:id', (req, res) => {
 
 // Numeron päivittäminen
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
+    const {name, number} = req.body
   
-    const person = {
-      name: body.name,
-      number: body.number,
-    }
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    Person.findByIdAndUpdate(
+        req.params.id,
+        { name, number }, 
+        { new: true, runValidators: true, context: 'query' })
       .then(updatedPerson => {
         res.json(updatedPerson)
       })
